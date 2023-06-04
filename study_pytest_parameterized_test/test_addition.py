29deead3caba2fbe0c501d603cc2049b4db3b18f
Luis Altuve <luis2ra@gmail.com>
2023-06-04 12:08:18 -0500

    updating tutorial

---
+++ study_pytest_parameterized_test/test_addition.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.parametrize("input1, input2, output", [(5,5,10),(3,5,12)])
+def test_add(input1, input2, output):
+	assert input1+input2 == output,"failed"
+
+'''
+For run test:
+$ pytest -k test_add -v
+'''
